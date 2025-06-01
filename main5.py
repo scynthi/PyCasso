dcd51086050f30c2f8713c8f53d51377001eeba4
adamsimon89 <discordplayclan@gmail.com>
2025-06-01 21:34:45 +0200

    Add files via upload

---
+++ main5.py
@@ -0,0 +1,39 @@
+import pygame, sys
+pygame.init()
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+canvas = pygame.Surface((WIDTH, HEIGHT))
+canvas.fill((255,255,255))
+shape, color, fill = "rect", (255, 0, 0), True
+drawing = False
+last_pos = None
+thickness = 4
+
+while True:
+    screen.blit(canvas, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP: thickness += 1
+            if event.key == pygame.K_DOWN and thickness > 1: thickness -= 1
+            if event.key == pygame.K_r: shape = "rect"
+            if event.key == pygame.K_e: shape = "ellipse"
+            if event.key == pygame.K_f: fill = not fill
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            drawing = True
+            last_pos = event.pos
+            start = event.pos
+        if event.type == pygame.MOUSEBUTTONUP:
+            drawing = False
+            last_pos = None
+            end = event.pos
+            rect = pygame.Rect(*start, end[0]-start[0], end[1]-start[1])
+            rect.normalize()
+            if shape == "rect":
+                pygame.draw.rect(canvas, color, rect, 0 if fill else 2)
+            else:
+                pygame.draw.ellipse(canvas, color, rect, 0 if fill else 2)
+        if event.type == pygame.MOUSEMOTION and drawing:
+            pygame.draw.line(canvas, (255,0,0), last_pos, event.pos, thickness)
+            last_pos = event.pos
+    pygame.display.update()
