8d1aca325aee9e5e106dde66b5500051e6201475
scynthi <85178488+scynthi@users.noreply.github.com>
2025-06-01 21:39:21 +0100

    Added history

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ WIDTH, HEIGHT = 600, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 canvas = pygame.Surface((WIDTH, HEIGHT))
 canvas.fill((255,255,255))
-shape = "rect"
-color = (255, 0, 0)
+history = []
+drawing = False
+last_pos = None
 
 while True:
     screen.blit(canvas, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_u and history:
+                canvas.blit(history.pop(), (0, 0))
+            if event.key == pygame.K_c:
+                history.append(canvas.copy())
+                canvas.fill((255,255,255))
         if event.type == pygame.MOUSEBUTTONDOWN:
-            start = event.pos
+            drawing = True
+            last_pos = event.pos
+            history.append(canvas.copy())
         if event.type == pygame.MOUSEBUTTONUP:
-            end = event.pos
-            rect = pygame.Rect(*start, end[0]-start[0], end[1]-start[1])
-            rect.normalize()
-            if shape == "rect":
-                pygame.draw.rect(canvas, color, rect)
-            else:
-                pygame.draw.ellipse(canvas, color, rect)
+            drawing = False
+            last_pos = None
+        if event.type == pygame.MOUSEMOTION and drawing:
+            pygame.draw.line(canvas, (0,0,0), last_pos, event.pos, 3)
+            last_pos = event.pos
     pygame.display.update()
